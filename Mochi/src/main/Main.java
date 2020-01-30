00d63a28d6d1f9b49a2acfe4e08b2965498a18ef
Phongphattharasiri <47464554+Phongphattharasiri@users.noreply.github.com>
2020-01-30 17:08:32 -0500

    FIle Modifications

---
+++ Mochi/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javafx.application.Application;
+import user_interface.StageConfig;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Application.launch(StageConfig.class, args);
+	}
+}
