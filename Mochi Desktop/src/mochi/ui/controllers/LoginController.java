9589283f837838672f02c12f0c6f3b8375472908
Phongphattharasiri <47464554+Phongphattharasiri@users.noreply.github.com>
2020-02-15 23:10:28 -0500

    usable login system

--- Mochi Desktop/src/mochi/ui/controllers/LoginController.java
+++ Mochi Desktop/src/mochi/ui/controllers/LoginController.java
@@ -2,30 +2,68 @@ package mochi.ui.controllers;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import mochi.db.DBConnection;
 import mochi.ui.RegistrationUI;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     public Pane pane;
-    public Text signUpText;
+    public Label signUpLabel;
+    public Label warningLabel;
     public Button loginButton;
     public TextField usernameField;
+    public TextField passwordField;
+
+    private Connection database;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loginButton.requestFocus();
+        this.database = DBConnection.getDatabase();
     }
 
-    public void signUpTextClick() {
+    public void signUpLabelClick() {
         Stage primaryStage = (Stage) pane.getScene().getWindow();
         RegistrationUI registrationUI = new RegistrationUI();
 
         primaryStage.setScene(registrationUI.getScene());
+    }
+
+    public void loginButtonClick() {
+        ResultSet resultSet = null;
+        Statement statement = null;
+
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        try {
+            statement = (Statement) database.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM `mochi-desktop`.user");
+
+            while(resultSet.next()) {
+                if (resultSet.getString(1).equals(username) &&
+                        resultSet.getString(2).equals(password)) {
+                    warningLabel.getStyleClass().add("Warning_Label_Success");
+                    warningLabel.setText("Welcome.");
+                }
+                else {
+                    warningLabel.getStyleClass().add("Warning_Label_Error");
+                    warningLabel.setText("You've enter a wrong username or password.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
