3b331012371aa21e2cb6b9012c3254186f1beee7
meaagaan <46685290+meaagaan@users.noreply.github.com>
2020-03-01 01:33:22 -0500

    Update VerifyEmail.java

--- Mochi Desktop/src/mochi/VerifyEmail.java
+++ Mochi Desktop/src/mochi/VerifyEmail.java
@@ -3,9 +3,13 @@ package mochi;
 import mochi.db.DBConnection;
 import mochi.ui.RegistrationUI;
 import java.sql.Connection;
+import java.util.Properties;
 
 // adjusts the value under "verified" in user table. default is 'new' -> 'verified' when user clicks email link
 public class VerifyEmail
 {
-
+    public static void sendVerificationEmail(String id, String email, String hash)
+    {
+        Properties properties = new Properties();
+    }
 }
