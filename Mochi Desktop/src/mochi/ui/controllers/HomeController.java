7da1e568d6f398c0f38780c35a670071f176e8b5
meaagaan <46685290+meaagaan@users.noreply.github.com>
2020-03-01 04:51:30 -0500

    update Home controller and UI

--- Mochi Desktop/src/mochi/ui/controllers/HomeController.java
+++ Mochi Desktop/src/mochi/ui/controllers/HomeController.java
@@ -1,4 +1,18 @@
 package mochi.ui.controllers;
 
-public class HomeController {
+import javafx.fxml.Initializable;
+import mochi.db.DBConnection;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable
+{
+    private Connection database;
+
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        this.database = DBConnection.getDatabase();
+    }
 }
