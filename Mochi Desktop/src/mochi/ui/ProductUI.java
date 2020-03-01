b92af1d2bfa6934a49d1fde83ef1ea71611dd4f9
alli25 <alisajohn98@gmail.com>
2020-03-01 17:27:32 -0500

    product controller, ui, stylesheet, and fxml created

---
+++ Mochi Desktop/src/mochi/ui/ProductUI.java
@@ -0,0 +1,30 @@
+package mochi.ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import mochi.ui.controllers.ProductController;
+
+import java.io.IOException;
+
+
+public class ProductUI {
+    private Scene productScene;
+    private ProductController productController;
+
+    public ProductUI() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxmls/ProductUI.fxml"));
+        Parent root = loader.load();
+
+        this.productController = loader.getController();
+        this.productScene = new Scene(root);
+    }
+
+    public Scene getLoginScene() {
+        return this.productScene;
+    }
+
+    public ProductController getProductController() {
+        return this.productController;
+    }
+}
