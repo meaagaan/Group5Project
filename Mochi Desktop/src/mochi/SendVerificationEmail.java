cb556727c736aede88618b8b2207099eb467cf53
meaagaan <46685290+meaagaan@users.noreply.github.com>
2020-03-01 02:43:14 -0500

    Added Javamail and implementing in SendVerificationEmail.java

--- Mochi Desktop/src/mochi/SendVerificationEmail.java
+++ Mochi Desktop/src/mochi/SendVerificationEmail.java
@@ -1,16 +1,28 @@
 package mochi;
 
-import mochi.db.DBConnection;
-import mochi.ui.RegistrationUI;
-import java.sql.Connection;
 import java.util.Properties;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
-// adjusts the value under "verified" in user table. default is 'new' -> 'verified' when user clicks email link
+// sends hashed verification email to new users
 public class SendVerificationEmail
 {
-    public static void sendEmail(String id, String email, String hash)
+    public static void sendLinkEmail(String id, String email, String hash)
     {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        // email server host is Gmail
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("mochidesktop@gmail.com", "NvaleHh1ovGKq1p1");
+            }
+        });
+
+
     }
 }
