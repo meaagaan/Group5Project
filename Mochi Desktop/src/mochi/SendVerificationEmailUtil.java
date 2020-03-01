5de2519e7b14ca5002fa6386b42542e017b2a335
meaagaan <46685290+meaagaan@users.noreply.github.com>
2020-03-01 03:45:58 -0500

    Update SendVerificationEmailUtil.java

---
+++ Mochi Desktop/src/mochi/SendVerificationEmailUtil.java
@@ -0,0 +1,54 @@
+package mochi;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+// sends hashed verification email to new users
+public class SendVerificationEmailUtil
+{
+    public static void sendLinkEmail(String id, String recipient, String hash) throws Exception
+    {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        // email server host is Gmail
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        // logs in to the mochi gmail account
+        String mochiEmail = "mochidesktop@gmail.com";
+        String mochiEmailPassword = "NvaleHh1ovGKq1p1";
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mochiEmail, mochiEmailPassword);
+            }
+        });
+
+        Message message = prepareMessage(session, mochiEmail, recipient);
+
+        Transport.send(message);
+
+    }
+
+    private static Message prepareMessage(Session session, String mochiEmail, String recipient)
+    {
+        try
+        {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(mochiEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject("User Email Verification");
+            message.setText("");
+            return message;
+        }
+        catch (MessagingException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
