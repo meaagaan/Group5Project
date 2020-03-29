792acd9bf0fa0c516bb3dd905a10fe2a48ca8ab7
Phongphattharasiri <47464554+Phongphattharasiri@users.noreply.github.com>
2020-03-29 00:51:44 -0400

    Add remove wishlist item function

--- Mochi Desktop/src/mochi/ui/controllers/WishlistController.java
+++ Mochi Desktop/src/mochi/ui/controllers/WishlistController.java
@@ -6,15 +6,19 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import mochi.User;
+import mochi.WishlistDatabase;
 import mochi.db.DBConnection;
 import mochi.ui.HomeUI;
 import mochi.ui.LibraryUI;
 import mochi.ui.ProfileUI;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.util.ResourceBundle;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WishlistController implements Initializable {
 	public Pane pane;
@@ -23,10 +27,12 @@ public class WishlistController implements Initializable {
 	public Label profileLabel;
 	public ListView wishList;
 	private Connection database;
-
+	private WishlistDatabase wishlistDatabase;
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		this.database = DBConnection.getDatabase();
+		this.wishlistDatabase = new WishlistDatabase(database);
+
 		if (!(User.getFirstname() == null)) {
 			titleLabel.setText(User.getFirstname() + "'s Wishlist");
 			profileLabel.setText(User.getFirstname());
@@ -34,16 +40,23 @@ public class WishlistController implements Initializable {
 		wishListFill();
 	}
 
-	public boolean deleteButtonClicked() {
-		Integer index = wishList.getSelectionModel().getSelectedIndex();
-
-		if (index != null) {
+	public boolean deleteButtonClicked() throws FileNotFoundException, SQLException {
+		int index = -1;
+		index = wishList.getSelectionModel().getSelectedIndex();
+		if (index >= 0) {
 			wishList.getItems().remove(index);
-			wishList.getItems().toArray();
+
+			List<Object> list = Arrays.asList(wishList.getItems().toArray());
+			List<String> stringList = list.stream()
+					.map(object -> Objects.toString(object, null))
+					.collect(Collectors.toList());
+
+			User.setWishlist((ArrayList) stringList);
+			this.wishlistDatabase.writeText(User.getWishlist());
+			this.wishlistDatabase.writeFile(User.getUsername());
+			return true;
 		}
-		else {
-			return false;
-		}
+		return false;
 	}
 
 	private boolean wishListFill() {
